0797945a4ea71e18689c4b01b910100a0d6e4aa1
labdacide <119441575+labdacide@users.noreply.github.com>
2023-04-09 19:16:14 +0200

    Update index.js

--- website/pages/home/index.js
+++ website/pages/home/index.js
@@ -161,6 +161,12 @@ export default function Home() {
               <p className="p-s"> THE FIRST CROSS-MEDIA COMMUNITY-POWERED ENTERTAINMENT COMPANY IN THE WEB3 INDUSTRY </p>
             </HeroTextIn>
           </h1>
+          (function(d, t){
+          if(window.location.hash!='#gleam'&&(''+document.cookie).match(/(^|;)\s*GleamFeI9S=X($|;)/)){return;}
+
+          var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+          g.src = "https://widget.gleamjs.io/FeI9S/ol.js"; s.parentNode.insertBefore(g, s);
+          }(document, "script"));
           <Button
             className={cn(s.cta, introOut && s.in)}
             arrow
