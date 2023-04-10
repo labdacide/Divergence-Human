e67b42b64065ef6ff070ab01aa2c7c30b2e8df09
labdacide <119441575+labdacide@users.noreply.github.com>
2023-04-10 02:28:44 +0200

    Update index.js

--- website/pages/home/index.js
+++ website/pages/home/index.js
@@ -139,8 +139,8 @@ export default function Home() {
             <HeroTextIn introOut={introOut}>
               <h2 className={cn('p-xs', s.tm)}>
                 <span>©</span> {new Date().getFullYear()} Human Divergence
-                <ConnectWallet accentColor="#00ffae" colorMode="#ff005f" />
               </h2>
+              <ConnectWallet accentColor="#00ffae" colorMode="#ff005f" />
             </HeroTextIn>
           </span>
         </div>
