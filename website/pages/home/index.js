ae44a83c0f1347f17cce27a6b3a3cea63897d26c
labdacide <119441575+labdacide@users.noreply.github.com>
2023-05-16 14:24:15 +0200

    Update index.js
    Remove Google Tab Management API tracker

--- website/pages/home/index.js
+++ website/pages/home/index.js
@@ -20,17 +20,6 @@ import s from './home.module.scss'
 import { Story } from 'components/feature-cards2'
 import { ConnectWallet } from "@thirdweb-dev/react";
 
-(function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'G-XS991R469Y');
-
 const center = {
   textAlign: 'center',
 }
